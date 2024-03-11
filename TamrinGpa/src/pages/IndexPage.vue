38926daffe27a1d0484e4f6b4ad33872814bd257
AmirHossein Takhti <amirhosseintakhti.at@gmail.com>
2024-03-11 15:04:42 +0330

    commit

--- TamrinGpa/src/pages/IndexPage.vue
+++ TamrinGpa/src/pages/IndexPage.vue
@@ -1,17 +1,36 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page >
+    <div class="row q-gutter-xs flex-center">
+      <div class="col-xs-8 col-md-10 q-ma-xs q-pa-xs">
+        <q-input
+        class="q-gutter-xs q-mt-xs"
+      v-model="text"
+      label="name"
+    />
+
+
+      </div>
+      <div class="col-xs-8 col-md-10 q-ma-xs q-pa-xs">
+        <q-input
+        class="q-gutter-xs q-mt-xs"
+      v-model="text"
+      label="grade"
+
+    />
+      </div>
+      <div class="col-7 marging flex flex-center">
+        <q-btn style="background: #FF0080; color: white;width: 100%;" label="avg" />
+      </div>
+    </div>
+
+
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'IndexPage'
-})
+  name: "IndexPage",
+});
 </script>
